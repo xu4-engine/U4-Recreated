b55abff25bddb2781ec6f35889d29a00cdf89645
Karl Robillard <wickedsmoke@users.sourceforge.net>
2022-06-02 12:37:04 -0400

    Import TMX maps.
    Converted by tmx-all-u4 from Workshop commit a3526b46.

---
+++ map/tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="tiles" tilewidth="16" tileheight="16" tilecount="256" columns="1">
+ <image source="tiles.png" width="16" height="4096"/>
+</tileset>
